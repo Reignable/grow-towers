243d33a97cd589cc79e8584ca9ab799800cebe11
Reignable <joebarret401@gmail.com>
2023-05-19 08:37:21 +0100

    Render tower data in app

--- apps/tower-monitor/src/app/app.component.ts
+++ apps/tower-monitor/src/app/app.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { Component, inject } from '@angular/core'
 import { RouterModule } from '@angular/router'
+import { TowerService } from '@grow-towers/simulation'
 
 @Component({
   standalone: true,
-  imports: [RouterModule],
+  imports: [CommonModule, RouterModule],
+  providers: [TowerService],
   selector: 'grow-towers-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'tower-monitor'
+
+  towerData$ = inject(TowerService).towerData$
 }
