d1f196ce6c75f5a2fb7d757a573ed41da841e16b
Reignable <joebarret401@gmail.com>
2023-05-19 13:29:41 +0100

    Format TowerService

--- libs/simulation/src/tower.service.ts
+++ libs/simulation/src/tower.service.ts
@@ -1,23 +1,24 @@
-import { BehaviorSubject, interval, map, Observable } from "rxjs";
-import { StaticDataFactory } from "./static-data-factory";
-import { Tower } from "./tower";
-import { TowerProgressSimulator } from "./tower-progress-simulator";
+import { interval, map, Observable } from 'rxjs'
+import { StaticDataFactory } from './static-data-factory'
+import { Tower } from './tower'
+import { TowerProgressSimulator } from './tower-progress-simulator'
+import { Slot } from './slot'
 
 export class TowerService {
-  public towerData$: Observable<Tower[]>;
+  public towerData$: Observable<Tower[]>
 
   constructor() {
-    let factory = new StaticDataFactory();
-    let towers = factory.build();
-    const simulator = new TowerProgressSimulator();
-    
+    let factory = new StaticDataFactory()
+    let towers = factory.build()
+    const simulator = new TowerProgressSimulator()
+
     this.towerData$ = interval(1000).pipe(
       map(() => {
-        const newTowers = simulator.incrementProgress(towers);
-        towers = newTowers;
+        const newTowers = simulator.incrementProgress(towers)
+        towers = newTowers
 
-        return newTowers;
-      }));
+        return newTowers
+      })
+    )
   }
 }
-
