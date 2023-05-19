fab68ec5a29c377892e28fc39ac5f6d6d3fe1422
Reignable <joebarret401@gmail.com>
2023-05-19 18:44:21 +0100

    Slots component cleanup

--- libs/slots/src/lib/slots/slots.component.ts
+++ libs/slots/src/lib/slots/slots.component.ts
@@ -1,15 +1,17 @@
 import { CommonModule } from '@angular/common'
 import { Component, inject } from '@angular/core'
+import { FormControl } from '@angular/forms'
 import { RouterLink } from '@angular/router'
 import { GrowthJobName, Slot, TowerService } from '@grow-towers/simulation'
 import { towerNumberRouteParam$ } from '@grow-towers/utils'
+import { orderBy } from 'lodash'
 import { CardModule } from 'primeng/card'
 import { combineLatest, map, startWith, switchMap } from 'rxjs'
-import { FormControl } from '@angular/forms'
-import { orderBy, sortBy } from 'lodash'
 import { SlotCardComponent } from '../slot-card/slot-card.component'
 import { GrowthJobFilterComponent } from './growth-job-filter/growth-job-filter.component'
 import { SlotsSortComponent } from './slots-sort/slots-sort.component'
+
+const trackBySlotNumber = (index: number, slot: Slot): number => slot.number
 
 @Component({
   selector: 'grow-towers-slots',
@@ -26,18 +28,18 @@ import { SlotsSortComponent } from './slots-sort/slots-sort.component'
   styleUrls: ['./slots.component.scss'],
 })
 export class SlotsComponent {
+  trackBySlotNumber = trackBySlotNumber
+
   towerNumber$ = towerNumberRouteParam$()
 
   towerService = inject(TowerService)
-
-  growthJobNameFilter = new FormControl<GrowthJobName | null>(null)
-
-  slotSort = new FormControl<string>('number', { nonNullable: true })
 
   slots$ = this.towerNumber$.pipe(
     switchMap(towerNumber => this.towerService.getTower(towerNumber)),
     map(tower => tower?.slots),
   )
+
+  growthJobNameFilter = new FormControl<GrowthJobName | null>(null)
 
   filteredSlots$ = combineLatest([
     this.slots$,
@@ -49,20 +51,15 @@ export class SlotsComponent {
     }),
   )
 
+  slotSort = new FormControl<string>('number', { nonNullable: true })
+
   sortedSlots$ = combineLatest([
     this.filteredSlots$,
     this.slotSort.valueChanges.pipe(startWith('number')),
   ]).pipe(
     map(([slots, slotSort]) => {
       if (!slotSort) return slots
-      if (slotSort === 'number' || slotSort === 'growthTray.growthJob.name') {
-        return sortBy(slots, slotSort)
-      }
-      return orderBy(slots, slotSort, 'desc')
+      return orderBy(slots, slotSort, slotSort.includes('Percentage') ? 'desc' : 'asc')
     }),
   )
-
-  trackBy(index: number, slot: Slot): number {
-    return slot.number
-  }
 }
