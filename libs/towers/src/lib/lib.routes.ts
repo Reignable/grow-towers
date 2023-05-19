28eaed639ad5e5f1c7f39daac0dc7b58d8bcb2dd
Reignable <joebarret401@gmail.com>
2023-05-19 11:51:25 +0100

    Add component for a tower to towers lib

--- libs/towers/src/lib/lib.routes.ts
+++ libs/towers/src/lib/lib.routes.ts
@@ -1,4 +1,8 @@
 import { Route } from '@angular/router'
 import { TowersComponent } from './towers/towers.component'
+import { TowerComponent } from './tower/tower.component'
 
-export const towersRoutes: Route[] = [{ path: '', component: TowersComponent }]
+export const towersRoutes: Route[] = [
+  { path: '', component: TowersComponent },
+  { path: ':towerNumber', component: TowerComponent },
+]
---
+++ libs/towers/src/lib/tower/tower.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core'
+import { CommonModule } from '@angular/common'
+
+@Component({
+  selector: 'grow-towers-tower',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './tower.component.html',
+  styleUrls: ['./tower.component.scss'],
+})
+export class TowerComponent {}
